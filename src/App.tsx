bc41153b0110a24821873ffbb067583ff080a125
scazan <scottcazan@gmail.com>
2018-07-31 17:32:59 +0200

    setup multi-canvas video grid

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,26 @@ class App extends React.Component {
 
   public constructor(props:any) {
     super(props);
-    this.videoCanvas = Array(3).fill(0).map( () => React.createRef() );
+    this.videoCanvas = Array(20).fill(0).map( () => React.createRef() );
     this.video = React.createRef();
   }
 
   public componentDidMount(): void {
-    console.log(this.video, this.videoCanvas);
-
-    this.videoCanvas.forEach( canvas =>
-      this.renderCanvas(canvas.current.getContext('2d'))
+    this.videoCanvas.forEach( ( canvas, i: any) =>
+      this.renderCanvas(canvas.current.getContext('2d'), i)
     );
 
     this.video.current.play();
   }
 
-  public renderCanvas(context: any) {
+  public renderCanvas(context: any, i: any) {
     const loop = () => {
-      context.drawImage(this.video.current, 0, 0);
+      const video = this.video.current;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+      const rowOffset = Math.floor(i/4) * (videoHeight/4);
+
+      context.drawImage(video, 0+((i%4)*(videoWidth/4)), rowOffset, (videoWidth/4), (videoHeight/4), 0 , 0, 100, 100);
       setTimeout( loop, 1000/30);
     };
 
@@ -32,15 +35,18 @@ class App extends React.Component {
   }
   public render() {
 
+    const canvases = this.videoCanvas.map( ref => {
+      return <canvas key={ref} ref={ref} width="100" height="100"/>
+    });
+
     return (
       <div className="App">
         <video controls={ true } ref={ this.video }>
           <source src="/video/video.webm" type="video/webm"/>
         </video>
 
-        <canvas ref={this.videoCanvas[0]} width="300" height="300"/>
-        <canvas ref={this.videoCanvas[1]} width="300" height="300"/>
-        <canvas ref={this.videoCanvas[2]} width="300" height="300"/>
+      {canvases}
+
       </div>
     );
   }
