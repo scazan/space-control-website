1af1995f6812605b143c845ddde24fc3def54fc2
scazan <scottcazan@gmail.com>
2018-07-31 16:45:37 +0200

    Rendering video to canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
+  private video: any;
+  private videoCanvas: any;
+  private canvasContext: any;
+
+  public constructor(props:any) {
+    super(props);
+    this.videoCanvas = React.createRef();
+    this.video = React.createRef();
+
+  }
+  public componentDidMount(): void {
+    console.log(this.video, this.videoCanvas);
+    const canvas: any = this.videoCanvas.current;
+
+    this.canvasContext = canvas.getContext('2d');
+
+    this.video.current.play();
+    this.renderCanvas();
+  }
+  public renderCanvas() {
+    console.log("hello");
+    const loop = () => {
+      this.canvasContext.drawImage(this.video.current, 0, 0);
+      setTimeout( loop, 1000/30);
+    };
+
+    loop();
+
+  }
   public render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <video controls={ true } ref={ this.video }>
+          <source src="/video/video.webm" type="video/webm"/>
+        </video>
+
+        <canvas ref={this.videoCanvas} width="300" height="300"/>
       </div>
     );
   }
