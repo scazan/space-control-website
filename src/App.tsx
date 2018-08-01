6d36819ca999511fd0ae82b98d08fd258c04a3b3
scazan <scottcazan@gmail.com>
2018-08-01 17:13:07 +0200

    Adding in background subtraction

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,36 @@ import './App.css';
 class App extends React.Component {
   private video: any;
   private videoCanvas: any[];
+  private videoBackgrounds: any[];
 
   public constructor(props:any) {
     super(props);
+    this.videoBackgrounds = Array(20).fill(0);
     this.videoCanvas = Array(20).fill(0).map( () => React.createRef() );
     this.video = React.createRef();
+
+    // Event Handler Binding
+    this.setBackground = this.setBackground.bind(this);
   }
 
   public componentDidMount(): void {
-    this.videoCanvas.forEach( ( canvas, i: any) =>
-      this.renderCanvas(canvas.current.getContext('2d'), i)
-    );
+    this.video.current.addEventListener('loadeddata', () => {
+      this.setBackground();
+      this.videoCanvas.forEach( (canvas, i: any) =>
+        this.renderCanvas(canvas.current.getContext('2d'), i)
+      );
 
-    // TODO: This only works on the second load because we need to load the video before playing
-    this.video.current.play();
+      this.video.current.play();
+    });
+  }
+
+  public setBackground() {
+    const video = this.video.current;
+    this.videoCanvas.forEach( (canvas, i: any) => {
+      const context = canvas.current.getContext('2d');
+      const imageData = context.getImageData(0, 0, video.videoWidth, video.videoHeight);
+      this.videoBackgrounds[i] = imageData;
+    });
   }
 
   public renderCanvas(context: any, i: any) {
@@ -28,7 +44,35 @@ class App extends React.Component {
       const rowOffset = Math.floor(i/4) * (videoHeight/4);
 
       context.drawImage(video, 0+((i%4)*(videoWidth/4)), rowOffset, (videoWidth/4), (videoHeight/4), 0 , 0, 100, 100);
-      setTimeout( loop, 1000/30);
+      // const imageData = context.getImageData(0+((i%4)*(videoWidth/4)), rowOffset, (videoWidth/4), (videoHeight/4));
+      const imageData = context.getImageData(0, 0, videoWidth, videoHeight);
+      const canvasBackground = this.videoBackgrounds[i];
+
+      const imageDataLength = imageData.data.length / 4;
+      for( let k=0; k < imageDataLength; k++) {
+        const r = imageData.data[k * 4 + 0];
+        const g = imageData.data[k * 4 + 1];
+        const b = imageData.data[k * 4 + 2];
+
+        const backgroundR = canvasBackground.data[k * 4 + 0];
+        const backgroundG = canvasBackground.data[k * 4 + 1];
+        const backgroundB = canvasBackground.data[k * 4 + 2];
+
+        if(
+          Math.abs(backgroundR - r) <= 50
+          && Math.abs(backgroundG - g) <= 50
+          && Math.abs(backgroundB - b) <= 50
+        ) {
+          imageData.data[k*4+3] = 0;
+        }
+        else {
+          // imageData.data[k*4+3] = 200;
+        }
+      }
+
+      context.putImageData(imageData, 0, 0);
+      // context.drawImage(video, 0, 0);
+      setTimeout(loop, 1000/30);
     };
 
     loop();
@@ -42,8 +86,9 @@ class App extends React.Component {
 
     return (
       <div className="App">
+      <button onClick={this.setBackground}>take background</button>
         <video controls={ true } ref={ this.video }>
-          <source src="/video/video.webm" type="video/webm"/>
+          <source src="/video/test.webm" type="video/webm"/>
         </video>
 
       {canvases}
