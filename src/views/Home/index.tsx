9e2a4f9e26eaf4b843f1642ecc43692a8f304c59
scazan <scottcazan@gmail.com>
2019-06-08 18:32:58 +0200

    Adding new Homepage with effects

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import Draggable from 'react-draggable';
 import { Scene,
   PlaneBufferGeometry,
@@ -6,17 +7,56 @@ import { Scene,
   Mesh,
   DoubleSide,
   PerspectiveCamera,
+  ShaderMaterial,
+  Vector2,
+  TextureLoader,
   WebGLRenderer
 } from 'three';
+import albumBackImage from './images/back_email.jpg';
+import albumFrontImage from './images/album_front.jpg';
+import fragmentShader from './frag.js';
+import vertexShader from './vert.js';
 
 import './index.css';
 
+let followMouse = false;
 const planets: React.RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>();
 const onHover = () => {
-  document.body.classList.add('active');
+  if(!followMouse) {
+    document.body.classList.add('active');
+
+    planets.current && (planets.current.style.position = 'fixed');
+    document.body.style.paddingBottom = '170px';
+    followMouse = true;
+  }
 };
 const onHoverOut = () => {
-  document.body.classList.remove('active');
+  // document.body.classList.remove('active');
+};
+
+let mouseX = '0px';
+let mouseY = '0px';
+let priorMouseX = '0px';
+let priorMouseY = '0px';
+
+const followMousePosition = (e) => {
+  priorMouseX =  mouseX;
+  priorMouseY = mouseY;
+
+  mouseX = `${e.clientX - 85}px`;
+  mouseY = `${e.clientY-85}px`;
+};
+
+const updateMouseFollower = () => {
+  if(followMouse) {
+    if(
+      planets.current
+      && ((mouseX !== priorMouseX) || (mouseY !== priorMouseY))
+    ) {
+      planets.current.style.left = mouseX;
+      planets.current.style.top = mouseY;
+    }
+  }
 };
 
 class Home extends React.Component {
@@ -25,11 +65,40 @@ class Home extends React.Component {
   }
 
   public componentDidMount() {
+    document.body.onmouseover = followMousePosition;
     const scene = new Scene();
-    const camera = new PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
-    camera.position.z = 2750;
-    const geometry = new PlaneBufferGeometry( 5, 20, 32 );
-    const material = new MeshBasicMaterial( {color: 0xffff00, side: DoubleSide} );
+    const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.001, 100);
+    camera.position.set(0, 0, 0.3)
+
+    const geometry = new PlaneBufferGeometry( 1, 1, 1, 1 );
+    // const material = new MeshBasicMaterial( {color: 0xfffff0, side: DoubleSide} );
+    const textureLoader = new TextureLoader();
+    const texture = textureLoader.load(albumBackImage);
+
+    let time = 0;
+    const material = new ShaderMaterial({
+      side: DoubleSide,
+      vertexShader,
+      fragmentShader,
+      uniforms: {
+        time: { type: 'f', value: time },
+        pixels: { type: 'v2', value: new Vector2(window.innerWidth, window.innerHeight) },
+        accel: { type: 'v2', value: new Vector2(0.5, 2) },
+        progress: { type: 'f', value: 1 },
+        uvRate1: {
+          value: new Vector2(1, 1)
+        },
+        texture1: {
+          value: texture
+        },
+        texture2: {
+          value: texture
+        }
+      }
+    })
+
+
+
     const plane = new Mesh( geometry, material );
 
     const renderer = new WebGLRenderer();
@@ -40,6 +109,16 @@ class Home extends React.Component {
     const container = document.querySelector('.backing');
     container && container.appendChild( renderer.domElement );
     scene.add( plane );
+
+    const renderGL = () => {
+      window.requestAnimationFrame(renderGL);
+      time += 0.05;
+      material.uniforms.progress.value += 0.000095;
+      updateMouseFollower();
+      renderer.render(scene, camera);
+    };
+
+    renderGL();
   }
 
   public render() {
@@ -47,21 +126,26 @@ class Home extends React.Component {
       <div className="home">
 
         <h2>Object Collection</h2>
+        <img className="albumCover" src={albumFrontImage}/>
         <h1>You Are Under Our Space Control</h1>
+
         <div className="info">
           <p>Thursday<br/>June 20<br/>7:30pm</p>
           <p>Friday<br/>June 21<br/>7:30pm</p>
           <p>Saturday<br/>June 22<br/>7:30pm</p>
         </div>
 
+        <a href="//web.ovationtix.com/trs/pr/1012213" target="_blank">
         <div className="tickets"
           onMouseOver={onHover}
           onMouseOut={onHoverOut}
+          ref={planets}
         >
           <a href="https://web.ovationtix.com/trs/pr/1012213" target="_blank">Tickets</a>
         </div>
+      </a>
 
-        <div className="backing" ref={planets}>
+        <div className="backing">
         </div>
       </div>
     );
